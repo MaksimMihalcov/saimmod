b6d54576bb542a025018aa69dbec08494c04dfe6
Maksim <Mikhatlsou>
2023-11-26 13:10:00 +0300

    fix

--- SAIMMOD3/SAIMMOD3/Program.cs
+++ SAIMMOD3/SAIMMOD3/Program.cs
@@ -1,11 +1,6 @@
 ﻿using SAIMMOD3;
 
 var tactsCount = 1000000;
-
-var lambda = 0.5;
-
-var isQueueBusy = false;
-
 var states = new Dictionary<string, double>()
 {
     { "2000", 1 },
@@ -21,56 +16,83 @@ var states = new Dictionary<string, double>()
 };
 
 var source = new Source();
+var p1 = 0.5;
+var p2 = 0.5;
 var channel1 = new Channel(0.5);
 var channel2 = new Channel(0.5);
+
 var request = false;
-var proc1 = true;
-var proc2 = true;
-var p = "";
+var isQueueBusy = false;
+var c1r = false;
 
 for (var i = 0; i < tactsCount; i++)
 {
-    proc1 = true;
-    proc2 = true;
-
     request = source.Tact();
+    
+    if (channel2.IsBusy)
+    {
+        if (channel2.Tact2(isQueueBusy))
+        {
+            if (isQueueBusy)
+                channel2.IsBusy = true;
+            isQueueBusy = false;
+        }
+    }
+    else
+    {
+        if (isQueueBusy)
+        {
+            channel2.IsBusy = true;
+            isQueueBusy = false;
+        }
+    }
 
-    if(!channel1.IsBusy && request)
+    if (request && !channel1.IsBusy)
     {
         channel1.IsBusy = true;
-        proc1 = false;
     }
-
-        if (proc1)
-            request = channel1.Tact1(request);
-
-        if(proc1)
-            isQueueBusy = isQueueBusy || request;
-
-        if(!channel2.IsBusy && isQueueBusy)
-        {
-            isQueueBusy = false;
-            channel2.IsBusy = true;
-            proc2 = false;
-        }
-
-     if(proc2)
-        channel2.Tact2(isQueueBusy);
-
-    if(!channel2.IsBusy && isQueueBusy)//
-    {    
-        isQueueBusy = false;
-        channel2.IsBusy = true;
-    }
-
-    states[$"{source.TactsToBid}{Convert.ToInt32(channel1.IsBusy)}{Convert.ToInt32(isQueueBusy)}{Convert.ToInt32(channel2.IsBusy)}"] += 1;
-    var w = $"{source.TactsToBid}{Convert.ToInt32(channel1.IsBusy)}{Convert.ToInt32(isQueueBusy)}{Convert.ToInt32(channel2.IsBusy)}";
-    if (w == "2111")
+    else
     {
-        var ww = 3;
+        c1r = channel1.Tact1(request);
+    
+        if (!isQueueBusy)
+        {
+            if (!channel2.IsBusy)
+                channel2.IsBusy = c1r;
+            else
+                isQueueBusy = c1r;
+        }
     }
-    p = $"{source.TactsToBid}{Convert.ToInt32(channel1.IsBusy)}{Convert.ToInt32(isQueueBusy)}{Convert.ToInt32(channel2.IsBusy)}";
+    
+    states[$"{source.TactsToBid}{Convert.ToInt32(channel1.IsBusy)}{Convert.ToInt32(isQueueBusy)}{Convert.ToInt32(channel2.IsBusy)}"] += 1;
 }
 
 foreach (var state in states)
-    Console.WriteLine($"{state.Key}  {state.Value/tactsCount}");
+    states[state.Key] = state.Value / tactsCount;
+
+var K1 = states["2100"] + states["1100"] + states["2101"] + states["1101"] + states["2111"] + states["1111"];
+var K2 = states["1001"] + states["2101"] + states["1101"] + states["2111"] + states["1011"] + states["1111"];
+var A = K2 * (1-p2);
+var lambda = 0.5;
+var Q = A/lambda;
+var Potk = 1 -  Q;
+var Loch = states["2111"] + states["1011"] + states["1111"];
+var Lc = states["1001"] + states["2100"] + states["1100"] + 
+         2*(states["2101"] + states["1101"] + states["1011"]) 
+         + 3*(states["2111"] + states["1111"]);
+var Woch = Loch / A;
+var Wc = Lc / lambda;
+
+foreach(var state in states)
+    Console.WriteLine($"{state.Key}  {state.Value}");
+Console.WriteLine(); 
+
+Console.WriteLine($"K1 {K1}");
+Console.WriteLine($"K2 {K2}");
+Console.WriteLine($"A {A}");
+Console.WriteLine($"Q {Q}");
+Console.WriteLine($"Potk {Potk}");
+Console.WriteLine($"Loch {Loch}");
+Console.WriteLine($"Lc {Lc}");
+Console.WriteLine($"Woch {Woch}");
+Console.WriteLine($"Wc {Wc}");
