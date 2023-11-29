eb05e874520ff309b27f284ea175674092a9eefd
Maksim <Mikhatlsou>
2023-11-29 23:11:39 +0300

    QQ

--- s4/s4/Program.cs
+++ s4/s4/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Diagnostics;
+
+class Program
 {
     static Dictionary<int, int> States = new Dictionary<int, int>();
 
@@ -10,43 +12,88 @@
 
         var queueLength = 0;
         var workingTime = DateTime.Now.AddSeconds(30);
-        var totalCount = 0;
-        
-        DateTime timeBeforeAppearance = DateTime.Now.AddSeconds(ExponentialDistribution(lambda));
-        DateTime processingTime = DateTime.Now;
+        long totalCount = 0;
 
+        DateTime processingTime = DateTime.Now;
+        DateTime timeBeforeAppearance = ExponentialDistribution(lambda);
+        
         while (DateTime.Now < workingTime)
         {
-            if (DateTime.Now > timeBeforeAppearance)
+            if (processingTime < DateTime.Now)
             {
-                if (DateTime.Now > processingTime && queueLength == 0)
+                if (queueLength > 0)
                 {
-                    processingTime = DateTime.Now.AddSeconds(ExponentialDistribution(mu));
-                    AddOrUpdateState(1);
+                    queueLength--;
+                    AddOrUpdateState(queueLength + 1);
+                    totalCount++;
+                    if (queueLength == 0)
+                        timeBeforeAppearance = ExponentialDistribution(lambda);
+                    processingTime = ExponentialDistribution(mu);
+                }
+                else
+                {
+                    if (timeBeforeAppearance < DateTime.Now)
+                    {
+                        AddOrUpdateState(0);
+                        totalCount++;
+                    }
+                    else
+                    {
+                        AddOrUpdateState(1);
+                        totalCount++;
+                        processingTime = ExponentialDistribution(mu);
+                        timeBeforeAppearance = ExponentialDistribution(lambda);
+                    }
+                }
+            }
+            else
+            {
+                if (timeBeforeAppearance < DateTime.Now)
+                {
+                    AddOrUpdateState(queueLength + 1);
+                    totalCount++;
                 }
                 else
                 {
                     queueLength++;
                     AddOrUpdateState(queueLength + 1);
-                }
-                timeBeforeAppearance = DateTime.Now.AddSeconds(ExponentialDistribution(lambda));
-            }
-
-            if (processingTime > DateTime.Now)
-            {
-                if (queueLength > 0)
-                {
-                    queueLength--;
-                    processingTime = DateTime.Now.AddSeconds(ExponentialDistribution(mu));
-                    AddOrUpdateState(queueLength + 1);
-                }
-                else
-                {
-                    AddOrUpdateState(0);
+                    totalCount++;
+                    timeBeforeAppearance = ExponentialDistribution(lambda);
                 }
             }
-            
-            totalCount++;
+            //timeBeforeAppearance = DateTime.Now.AddSeconds(ExponentialDistribution(lambda));
+            // if (DateTime.Now >= timeBeforeAppearance)
+            // {
+            //     if (DateTime.Now > processingTime && queueLength == 0)
+            //     {
+            //         processingTime = ExponentialDistribution(mu);
+            //         AddOrUpdateState(1);
+            //         totalCount++;
+            //         continue;
+            //     }
+            //     else
+            //     {
+            //         queueLength++;
+            //         AddOrUpdateState(queueLength + 1);
+            //     }
+            //     timeBeforeAppearance = ExponentialDistribution(lambda);
+            // }
+            //
+            // if (processingTime > DateTime.Now)
+            // {
+            //     if (queueLength > 0)
+            //     {
+            //         queueLength--;
+            //         processingTime = ExponentialDistribution(mu);
+            //         AddOrUpdateState(queueLength + 1);
+            //     }
+            //     else
+            //     {
+            //         AddOrUpdateState(0);
+            //     }
+            // }
+            //
+            // totalCount++;
         }
 
         foreach (var state in States)
@@ -64,10 +111,10 @@
         Console.WriteLine("Ts " + Ts);
     }
     
-    static double ExponentialDistribution(double val)
+    static DateTime ExponentialDistribution(double val)
     {
         var rand = new Random();
-        return -Math.Log(rand.NextDouble()) / val;
+        return DateTime.Now.AddSeconds(-Math.Log(rand.NextDouble()) / val);
     }
     
     static bool ToReService(double p)
